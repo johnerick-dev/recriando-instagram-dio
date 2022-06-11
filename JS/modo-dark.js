e0e1194112cdba13c7925f294c1888cb1f7d89eb
John Erick <johnerick.dev@outlook.com>
2022-06-11 19:27:43 -0300

    js e responsivo do botao modo dark

--- JS/modo-dark.js
+++ JS/modo-dark.js
@@ -1,14 +1,22 @@
-function mudarTema(id) { 
-    if (id == "light" ) {
+function mudarTemaLight(id) { 
+    if (id == "light") {
         document.getElementsByTagName("body")[0].style.background = "#fff";
         document.querySelector(".link-facebook").style.color = "#3b5998";
+        document.querySelector(".obter-app").style.color = "#000";
+        document.querySelector(".nao-tem-conta").style.color = "#000";
         document.querySelector(".foto").src = "./imagens/celular-white.png"
-    } else {
-        document.getElementsByTagName("body")[0].style.background = "#000";
-        document.querySelector(".link-facebook").style.color = "#fff";
-        document.querySelector(".foto").src = "./imagens/celular-dark.png"
     }    
 }
-mudarTema();
+mudarTemaLight();
+
+function mudarTemaDark (id) {
+    if (id == "dark") 
+    document.getElementsByTagName("body")[0].style.background = "#000";
+    document.querySelector(".link-facebook").style.color = "#fff";
+    document.querySelector(".obter-app").style.color = "#fff";
+    document.querySelector(".nao-tem-conta").style.color = "#ccc";
+    document.querySelector(".foto").src = "./imagens/celular-dark.png"
+} 
+mudarTemaDark();
 
 
